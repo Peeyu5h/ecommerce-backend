04679b02920d6bb8be1af6116b487f690c7b7280
Peeyu5h <piyu15sh@gmail.com>
2026-08-02 17:14:36 +0530

    move wishlist to cart implemented

--- src/routes/wishlist.routes.js
+++ src/routes/wishlist.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authMiddleware from "../middlewares/auth.middleware.js";
-import { addToWishList, deleteWishList, getWishListItems, removeWishListItem } from "../controllers/wishlist.controller.js";
+import { addToWishList, deleteWishList, getWishListItems, moveAllToCart, removeWishListItem } from "../controllers/wishlist.controller.js";
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.post("/", authMiddleware, addToWishList);
 router.get("/", authMiddleware, getWishListItems);
 router.delete("/", authMiddleware, deleteWishList);
 router.delete("/:id", authMiddleware, removeWishListItem);
+router.post("/move-to-cart", authMiddleware, moveAllToCart);
 
 export default router;
